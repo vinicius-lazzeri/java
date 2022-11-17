cb2eadd22fcddffdd475e71f6a391f374a88bc96
Vinicius Lazzeri <vinicius.lazzeri@hotmail.com>
2022-11-17 12:12:14 -0400

    update

---
+++ estudos/tipos-de-variaveis/src/operadores/Operadores.java
@@ -0,0 +1,18 @@
+package operadores;
+
+//import java.sql.Date;
+
+public class Operadores {
+    //String nome = "Vini";
+    //int idade = 24;
+    //double peso = 68.5;
+    //char sexo = 'M';
+    //boolean doadorOrgao = false;
+    //Date dataNascimento = new Date();
+
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM" + " " + "JAVA";
+        System.out.println(nomeCompleto);
+    }
+    
+}
---
+++ estudos/tipos-de-variaveis/src/operadores/Repeticao.java
@@ -0,0 +1,12 @@
+package operadores;
+
+public class Repeticao {
+    public static void main(String[] args) {
+        
+        int numero = 5;
+
+        numero += numero;
+
+        System.out.println(numero);
+    }
+}
