b017be0b23d4b85731e0728d88ac4e3249f79e14
Vinicius Lazzeri <vinicius.lazzeri@hotmail.com>
2022-11-17 12:41:39 -0400

    update

--- estudos/tipos-de-variaveis/src/operadores/Repeticao.java
+++ estudos/tipos-de-variaveis/src/operadores/Repeticao.java
@@ -1,12 +1,22 @@
 package operadores;
 
+import javax.xml.bind.SchemaOutputResolver;
+
 public class Repeticao {
     public static void main(String[] args) {
         
-        int numero = 5;
+        //int numero = 5;
 
-        numero += numero;
+        //numero += 1;
+        //numero++;
 
-        System.out.println(numero);
+        //System.out.println(numero--);
+        //System.out.println(numero);
+
+        boolean variavel = true;
+
+        variavel = !variavel;
+        
+        System.out.println(variavel);
     }
 }
