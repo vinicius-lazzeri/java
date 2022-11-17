785481fe85dff44cd81aa7d96f4ce95f21208d1f
Vinicius Lazzeri <vinicius.lazzeri@hotmail.com>
2022-11-17 10:46:33 -0400

    update

---
+++ estudos/tipos-de-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,16 @@
+public class TiposVariaveis {
+    public static void main(String[] args) {
+            //tipos primitivos
+            //estudar a classe String que representa texto na aplicação;
+        
+            byte idade = 123;
+            short ano = 2021;
+            int cep = 21070333;
+            long cpf = 98765432109L;
+            float pi = 3.14F;
+            double salario = 1275.33;   
+            
+            final double VALOR_DE_PI = 3.14;
+
+    }
+}
